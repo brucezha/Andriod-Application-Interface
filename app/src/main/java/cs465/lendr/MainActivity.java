0dd3cbf984a769b3135dc98046881a5a505bae2e
tracy <Supertracy@tracys-MacBook-Pro.local>
2016-11-15 00:38:28 +0800

    first submission

--- app/src/main/java/cs465/lendr/MainActivity.java
+++ app/src/main/java/cs465/lendr/MainActivity.java
@@ -3,11 +3,45 @@ package cs465.lendr;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity{
+    //step1 : create edit text variable
+    private EditText et;
+    private Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //step2 : fetch edit text from xml file.
+        et = (EditText) findViewById(R.id.Location);
+        b = (Button) findViewById(R.id.button1);
+
+        b.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //step3 : set the text to edit text by using setText function
+                //et.setText("Button is clicked..");
+                et.setFocusable(true);
+                et.setCursorVisible(true);
+                et.setClickable(true);
+                et.isEnabled();
+                et.setFocusableInTouchMode(true);
+//                et.setFocusable(true);
+//                et.setEnabled(true);
+//                et.setClickable(true);
+//                et.setFocusableInTouchMode(true);
+            }
+        });
     }
+
 }
